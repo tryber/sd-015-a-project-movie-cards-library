2fe3e596d19745284780f300f0c47a3f2519a8e1
Guilherme Polippo <luispolippo@hotmail.com>
2021-10-05 18:57:37 -0300

    Implementado requisitos 3 a 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,25 @@
 // implement MovieCard component here
+import React, { Component } from 'react';
+import propTypes from 'prop-types';
+
+class MovieCard extends Component {
+  render() {
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
+    return (
+      <section>
+        <img src={ imagePath } alt={ title } />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+      </section>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  title: propTypes.string.isRequired,
+  subtitle: propTypes.string.isRequired,
+  storyline: propTypes.string.isRequired,
+  imagePath: propTypes.string.isRequired,
+  rating: propTypes.number.isRequired,
+};
