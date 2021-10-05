afcab90379f1b7954c22d805a2c2d3aeac21bdab
Rafael Augusto Scherer <rafaelaugustscherer@gmail.com>
2021-10-05 20:18:34 -0300

    Todos os requisitos - base

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,33 @@
-// implement MovieCard component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
+
+class MovieCard extends Component {
+  render() {
+    const { key } = this.props;
+    const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
+
+    return (
+      <div key={ key }>
+        <img src={ imagePath } alt="Movie" />
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+        <Rating rating={ rating } />
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  key: PropTypes.string.isRequired,
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+export default MovieCard;
