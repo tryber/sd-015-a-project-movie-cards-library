261c9423ca4efe50b7828210b477c5ffd8df38de
Leandro <play3rtox1c@gmail.com>
2021-10-05 16:00:50 -0300

    commitao bomba

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,29 @@
 // implement MovieList component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+    const elements = movies
+      .map((movie) => (<MovieCard key={ movie.title } movie={ movie } />));
+    return (
+      <div>
+        { elements }
+      </div>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  })).isRequired,
+};
+
+export default MovieList;
