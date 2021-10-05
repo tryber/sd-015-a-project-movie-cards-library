6fbc43f1db31164956f8388ad1feaf611e4c0b82
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-10-05 19:55:00 -0400

    Soluçao do requisito 1 e 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,16 @@
 // implement Header component here - FIRST COMMIT - Iniciando o projeto - VAMO COM TUDO! PRA CIMA!
+import React from 'react';
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (<header>
+      <h1>Movie Cards Library</h1>
+    </header>
+    )
+  }
+}
+
+export default Header;
