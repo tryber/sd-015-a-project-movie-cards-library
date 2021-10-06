db3cb881d28e57c536c410920d2155a45747bb54
Gabriel <76435193+gncorreia@users.noreply.github.com>
2021-10-06 16:31:51 -0300

    requirement 3, 4 and 5 done

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,27 @@
 // implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie: { title, subtitle } } = this.props;
+    return (
+      <div className="movie-card movie-card-body">
+        <p className="movie-card-title">{ title }</p>
+        <p className="movie-card-subtitle">{ subtitle }</p>
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    retina: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,30 @@
 // implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div className="movie-list">
+        {movies.map((movie) => <MovieCard movie={ movie } key={ movie.title } />)}
+      </div>
+    );
+  }
+}
+// source: https://stackoverflow.com/questions/54155412/error-map-in-missing-props-reactjs-proptypes
+// and: https://stackoverflow.com/questions/64012257/proptype-name-is-not-required-but-has-no-corresponding-defaultprops-declarati
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({ // aparentemente pode usar o objectOf tbm
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      retina: PropTypes.number,
+      imagePath: PropTypes.string,
+    }),
+  ).isRequired,
+};
+
+export default MovieList;
