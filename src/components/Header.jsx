57eae7e0306f8a47741e0d0b25ba9b7f0d67aa59
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-10-05 20:10:24 -0400

    Soluçao do requisito 3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,6 @@
 import React from 'react';
 
 class Header extends React.Component {
-  constructor(props) {
-    super(props)
-  }
   render() {
     return (<header>
       <h1>Movie Cards Library</h1>
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,18 @@
 // implement MovieList component here
+import React from "react";
+
+class MovieList extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    const { movies } = this.props;
+    return (
+      <section>
+        <ul></ul>
+      </section>
+    )
+  }
+}
+
+export default MovieList;
