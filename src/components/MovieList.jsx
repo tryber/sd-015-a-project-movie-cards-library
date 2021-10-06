18f42ce9df8be1ff7af8a8d8f992e953f46d7423
Vitor Diorio <vitor.diorio@yahoo.com.br>
2021-10-06 16:57:31 -0300

    Lint

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,11 +4,6 @@ import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 export default class MovieList extends React.Component {
-  // eslint-disable-next-line no-useless-constructor
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     const { movies } = this.props;
     return (
