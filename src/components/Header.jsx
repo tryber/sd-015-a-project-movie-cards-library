018747a270067bbe0b3e437fd9e91ff70708d65d
Lucas Macedo <lucasrrm22@gmail.com>
2021-10-06 20:39:16 -0300

    requisito 1 e aparentemente o 15 tbm

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,2 +1,15 @@
 // implement Header component here
 // primeiro commit
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Movie Cards</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
