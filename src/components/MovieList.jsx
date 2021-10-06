41d8de5f7997e3b4c7b21de2c5312b27a2261345
Igor Campos Moreira <igorcampos200@gmail.com>
2021-10-06 19:13:41 -0300

    Requisito 3

---
+++ src/components/MovieList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div className="movie-list">
+        <p>aqui</p>
+      </div>
+    );
+  }
+}
+export default MovieList;
