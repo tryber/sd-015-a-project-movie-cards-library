b5dd92965d56b34bd3e12bbc03c2196e492022ac
Leandro Basilio <leandroab07@gmail.com>
2021-10-06 16:10:39 -0300

    Adiciona requisito 4

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,11 @@
 // implement MovieList component here
+// implement Header component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (<div>movies</div>);
+  }
+}
+
+export default MovieList;
