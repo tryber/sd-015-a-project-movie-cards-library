958c2753b11fa61661acbcf785c99665bad29bcc
Alex Sandro Sousa <alexsandro091092@gmail.com>
2021-10-06 16:29:39 -0300

    teste

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,31 @@
-// implement MovieCard component here
+import React from 'react';
+import Rating from './Rating';
+import data from '../data';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      data.map((element) => (
+        <div key={ element.title }>
+          <div>
+            <h4>{element.title}</h4>
+            <h5>{element.subtitle}</h5>
+            <p>{element.storyline}</p>
+            <img src="{element.imagePath}" alt="Algo" />
+            <div>
+              <Rating />
+            </div>
+          </div>
+        </div>
+      ))
+    );
+  }
+}
+
+// movies.prototype = {
+//   title: PropTypes.string,
+//   subtitle: PropTypes.string,
+//   storyline: PropTypes.string,
+//   imagePath: Proptypes.string,
+// };
+export default MovieCard;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,22 @@
-// implement Rating component here
+import React from 'react';
+import data from '../data';
+
+class Rating extends React.Component {
+  render() {
+    // const { rating } = this.props;
+
+    return (
+      <div>
+        {
+          data.map((el) => el.rating)
+        }
+      </div>
+    );
+  }
+}
+
+// Rating.propTypes = {
+//   rating: movies.string,
+// };
+
+export default Rating;
