63d8ceef932bb4d78c50380ab03624d6a0742dc8
bruholanda <brunaholandacunha7@gmail.com>
2021-10-06 16:28:55 -0300

    Requirements from 7 to 10;

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
     return (
-      <div>
-        Passar no lint
-      </div>
+      <>
+        <img src={ movie.imagePath } alt="Imagem do Filme" />
+        <h4>{ movie.title }</h4>
+        <h5>{ movie.subtitle }</h5>
+        <p>{ movie.storyline }</p>
+      </>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.objectOf().isRequired,
+};
 
 export default MovieCard;
