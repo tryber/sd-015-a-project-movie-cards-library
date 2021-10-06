e94fc8402cffab37c0b3d8481320fd9c5fa6c80b
neandermenezes <neander2801@gmail.com>
2021-10-06 11:41:40 -0300

    acho que foi 10 nao tenho certeza

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
-      <div className="movie-title" />
+      <section className="movie-list">
+        <div>
+          { movies.map((elem) => (<MovieCard
+            key={ elem.title }
+            movie={ elem }
+          />))}
+        </div>
+
+      </section>
     );
   }
 }
