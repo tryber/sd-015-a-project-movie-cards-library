4ed4702134b26df62bf7768bc1c8719686d20ddd
Lucas Pontes Arruda <paluke94@gmail.com>
2021-10-07 00:46:44 +0200

    Requisitos MovieList e MovieCard (3 ao 10)

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        { movies.map((film) => <MovieCard movie={ film } key={ film.title } />)}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
