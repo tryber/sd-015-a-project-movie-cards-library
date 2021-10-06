714ad87a939ddb64187135a2a7ba17338ce58744
João V. Dias <diasvictorj@gmail.com>
2021-10-06 17:45:52 -0300

    Terceiro requisito incompleto

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,22 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+
+  render() {
+    const { data } = this.props;
+    return (
+      <div>
+        { data.map((movieElement) => (
+          <MovieCard
+            name={ movieElement }
+            key={ movieElement.title }
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
