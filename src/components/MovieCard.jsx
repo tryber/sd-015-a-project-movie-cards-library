da9f8c809128ee8f89f45bf8c3d335cdc892edb4
Braien <braienmp@outlook.com>
2021-10-06 16:41:34 -0300

    renderiza a imagem do filme dentro de uma tag img

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,25 @@
 // implement MovieCard component here
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
-    return <p>Teste</p>;
+    const { movie: {title, subtitle, storyline, imagePath, rating}} = this.props;
+    return (
+      <div>
+        <img src={ imagePath } alt="Movie cover" />
+      </div>);
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -8,7 +8,7 @@ class MovieList extends Component {
     const { movies } = this.props;
     return (
       <div>
-        {movies.map((movie) => <MovieCard key={ movie.title } />)}
+        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>);
   }
 }
