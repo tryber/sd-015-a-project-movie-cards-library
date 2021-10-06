4f45d7bdb2d2f6a248526ab33a298b4f2f1ad1c5
Gabriel <76435193+gncorreia@users.noreply.github.com>
2021-10-06 17:43:48 -0300

    lint and css fixed

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,8 +7,8 @@ class MovieCard extends React.Component {
   render() {
     const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
     return (
-      <div className="movie-card movie-card-body">
-        <img src={ imagePath } alt="" />
+      <div className="movie-card">
+        <img src={ imagePath } alt="" className="movie-card-image" />
         <h4 className="movie-card-title">{ title }</h4>
         <h5 className="movie-card-subtitle">{ subtitle }</h5>
         <p className="movie-card-storyline">{ storyline }</p>
