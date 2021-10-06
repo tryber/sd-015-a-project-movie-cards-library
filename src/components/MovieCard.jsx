592c452d9ed77399e322c8fdec774f2f2cefdab3
Rodrigo Lima <limarodrigoo@outlook.com>
2021-10-06 16:08:00 -0300

    create MovieCard component

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,12 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render(){
+    const {movie: {title, subtitle, storyline, imagePath, rating}}
+    return(
+      
+    )
+  }
+}
+
+export default MovieCard 
