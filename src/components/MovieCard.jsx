1b045529aee9447a9669336636b7ce60c33e96f0
Gustavo Lima <lima_gus@outlook.com>
2021-10-06 17:23:02 -0300

    Adiciona propTypes em MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import propTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
     //  Essa prop é um objeto contendo as propriedades: title, subtitle, storyline, imagePath e rating.
-    const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
+    const { movie: { title, subtitle, storyline, imagePath } } = this.props;
 
     return (
       <div>
-        <img src={ imagePath } alt={title} />
+        <img src={ imagePath } alt={ title } />
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
         <p>{ storyline }</p>
@@ -16,5 +16,15 @@ class MovieCard extends React.Component {
     );
   }
 }
+
+// propTypes
+MovieCard.propTypes = {
+  movie: propTypes.shape({
+    title: propTypes.string,
+    subtitle: propTypes.string,
+    storyline: propTypes.string,
+    imagePath: propTypes.string,
+  }).isRequired,
+};
 
 export default MovieCard;
