8a1effed8fc5a664d979d799d1f543521144d8f0
gabriel <gabriel.f.takaki@gmail.com>
2021-10-06 17:05:11 -0300

    criando os cards

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,14 +3,14 @@ import PropTypes from 'prop-types';
 
 class MovieCard extends Component {
   render() {
-    const { movie: { title, subtitle, storyline, imagePath } } = this.props;
+    const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
     return (
       <section>
         <img src={ imagePath } alt="" />
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
         <p>{ storyline }</p>
-        {/* <Rating rating={ rating } /> */}
+        <Rating rating={ rating } />
       </section>
     );
   }
