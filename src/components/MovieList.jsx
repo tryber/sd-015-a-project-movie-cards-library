3a1e0107ebcbf00eaf90898d36dd394445036a55
neandermenezes <neander2801@gmail.com>
2021-10-06 12:40:22 -0300

    feitogit add .

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,21 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
+    console.log(movies);
     return (
-      <section className="movie-list">
-        <div>
-          { movies.map((elem) => (<MovieCard
-            key={ elem.title }
-            movie={ elem }
-          />))}
-        </div>
-
-      </section>
+      <main className="movie-list">
+        { movies.map((elem) => (<MovieCard key={ elem.title } movie={ elem } />))}
+      </main>
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      rating: PropTypes.number,
+      imagePath: PropTypes.string,
+      link: PropTypes.string,
+    }),
+  ),
+};
+
+MovieList.defaultProps = {
+  movies: [],
+};
 
 export default MovieList;
