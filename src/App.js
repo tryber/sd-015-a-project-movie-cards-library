4dc338fd84507f50e984aaa60fadfcdf4ca15dfb
ricardo-zabir <ricardofonseca.zabir@hotmail.com>
2021-10-06 14:58:46 -0300

    Ajuste

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
-      Hora de coda! 🚀✍️👨‍💻👩‍💻💪
+      {
+        
+      }
     </div>
   );
 }
