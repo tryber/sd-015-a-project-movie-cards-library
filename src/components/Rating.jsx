e85989aebbeaffa5680fe79f99c6aedd435a8e78
alexandrexpj <alexandrexpjr@hotmail.com>
2021-10-06 15:52:26 -0300

    requisito 11

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,11 @@
 // implement Rating component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Rating extends React.Component {
+  render() {
+
+  }
+}
+
+export default Rating;
