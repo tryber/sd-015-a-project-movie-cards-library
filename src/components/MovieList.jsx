a81b221c3af5ab931bd14dfb4036f3f4049c5b61
Gabriel Carvalho Maia <maiagabrielc@gmail.com>
2021-10-06 14:32:21 -0300

    requisitos 1-3 e 15-16 ok

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,11 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+export default function MovieList() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
