9afc41fa3edc25fc90600feadec04eb31a39100a
Guilherme Zobel <guilherme.zobel@gmail.com>
2021-10-06 20:23:45 -0300

    requisito 3, 4, 5

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <div>
+      <div className="movie-card-header">
         <header>
-          <h1>Movie Cards Library</h1>
+          <h1>Movie Cards  Library</h1>
         </header>
       </div>
     );
