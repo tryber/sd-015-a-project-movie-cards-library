756985c299e0dbb70c3cebfc3e9ed5c0ea417773
Renato Moreno <renats.mc@gmail.com>
2021-10-06 19:46:03 -0300

    requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
 // implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <section>
+        Lista de Filmes
+        { movies.title }
+      </section>
+    );
+  }
+}
+
+export default MovieList;
