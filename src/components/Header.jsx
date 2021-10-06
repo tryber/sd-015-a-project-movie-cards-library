59e92dfe334f9003a71da7f096c7482e68fa144f
Gabriel Moda <gabriel-moda@hotmail.com.br>
2021-10-06 15:02:47 -0300

    1,2 e 15

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,16 @@
 // implement Header component here
+import React from 'react';
+// import movies from '../data';
+import '../App.css';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
