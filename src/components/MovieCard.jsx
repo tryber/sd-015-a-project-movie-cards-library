d55764df26716245b0c285550393d7114baacc81
rafaelmedeiross <rafael.medeiros@msn.com>
2021-10-06 18:03:03 -0300

    Requisito 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import Rating from './Rating';
 // import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
-    const { movie: { title, subtitle, storyline, imagePath } } = this.props;
+    const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
     return (
       <div>
         <img src={ imagePath } alt="movie card" />
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+        <Rating rating={ rating } />
       </div>
     );
   }
