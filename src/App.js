28ba1e5fb95a4f68e1dce3a3c594b11d00dc5c85
Monte <you@example.com>
2021-10-06 17:49:01 -0300

    commit bomba, esqueci de ir fazendo commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-import movieList from './components/MovieList';
+import MovieList from './components/MovieList';
+import movies from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <movieList />
+      <MovieList movies={ movies } />
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
-class movieList extends React.Component {
+class MovieList extends React.Component {
   render() {
+    const { movies } = this.props
     return (
-      <div>
-        <MovieCard />
-      </div>
+      <section className="movie-list">
+        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+      </section>
     );
   }
 }
 
-export default movieList;
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  })).isRequired,
+};
+
+export default MovieList;
