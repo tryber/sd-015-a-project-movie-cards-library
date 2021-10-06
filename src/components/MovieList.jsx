a1fa1bc7bad5eccaefb3224e53245c4e297fb0ed
Tamyres <tamyrescaroline.oliveira@gmail.com>
2021-10-06 15:31:32 -0300

    feat: add requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+
+class MovieList extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default MovieList;
