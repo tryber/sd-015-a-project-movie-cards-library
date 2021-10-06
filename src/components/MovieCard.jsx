ccfe4c46c750dac93f866bbcfe7ed8bf6276d638
alvarogularte <alvaro.gularte@gmail.com>
2021-10-06 16:16:16 -0300

    Requirements 11, 12, 13 & 14 successfully completed

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
+
     return (
       <section>
         <img src={ movie.imagePath } alt="Imagem ilustrativa do filme" />
         <h4>{ movie.title }</h4>
         <h5>{ movie.subtitle }</h5>
         <p>{ movie.storyline }</p>
+        <Rating rating={ movie.rating } />
       </section>
     );
   }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -5,6 +5,7 @@ import MovieCard from './MovieCard';
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
+
     return (
       <section>
         <ul>
