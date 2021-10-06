2ed425a6902dd70b012a272a7d8a67944ff82708
Valdeci <moura97.valdeci@gmail.com>
2021-10-06 17:52:11 -0300

    Aplica css previo com leves mudanças e implementa Rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    const { movie: { title, subtitle, storyline, imagePath } } = this.props;
+    const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
     return (
-      <section>
-        <img src={ imagePath } alt={ title } />
-        <h2>{title}</h2>
-        <p>{subtitle}</p>
-        <p>{storyline}</p>
+      <section className="movie-card movie-card-body">
+        <img className="movie-card-image" src={ imagePath } alt={ title } />
+        <h4 className="movie-card-title">{title}</h4>
+        <h5 className="movie-card-subtitle">{subtitle}</h5>
+        <p className="movie-card-storyline">{storyline}</p>
+        <Rating rating={rating}/>
       </section>
     );
   }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -5,9 +5,13 @@ import MovieCard from './MovieCard';
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
-
+ 
     return (
-      movies.map((movie) => (<MovieCard movie={ movie } key={ movie.title } />))
+      <div className="movie-list">
+        {
+        movies.map((movie) => (<MovieCard movie={ movie } key={ movie.title } />))
+        }
+      </div>
     );
   }
 }
