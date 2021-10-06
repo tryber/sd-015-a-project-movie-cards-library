2d41cdc4dd1bc867dea9540a0d01938943071a49
Rodrigo Lima <limarodrigoo@outlook.com>
2021-10-06 16:13:48 -0300

    render img, title, subtitle, storyline

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
-  render(){
-    const {movie: {title, subtitle, storyline, imagePath, rating}}
-    return(
-      
-    )
+  render() {
+    const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
+    return (
+      <div>
+        <img src={ imagePath } alt={ title } />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+      </div>
+    );
   }
 }
 
-export default MovieCard 
+const propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
+
+MovieCard.propTypes = propTypes;
+export default MovieCard;
