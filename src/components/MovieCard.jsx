36d46cbf634713e68ec4ca03731f7ec1a9cb471b
thomazj <thomaz.jefferson01@protonmail.com>
2021-10-06 17:35:39 -0300

    requisito 4, 5 e 6

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,21 @@
 // implement MovieCard component here
+import React from 'react';
+import movies from '../data';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      movies.map((movie) => (
+        <ul key={ movie.title }>
+          <img src={ movie.imagePath } alt="filmeImg" />
+          <li>{ movie.title }</li>
+          <li>{ movie.subtitle }</li>
+          <li>{ movie.storyline }</li>
+          <li>{ movie.rating }</li>
+        </ul>
+      ))
+    );
+  }
+}
+
+export default MovieCard;
