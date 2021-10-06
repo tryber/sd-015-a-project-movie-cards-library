bb9e1c30fbc5937a4d5d6596de7d04ca5bb13bf5
alexandrexpj <alexandrexpjr@hotmail.com>
2021-10-06 15:36:30 -0300

    requisito 6 e 7

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,11 +4,11 @@ import React from 'react';
 class MovieCard extends React.Component {
   render() {
     const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
-    return(
+    return (
       <div className="movie-card">
-        
+        <img src={imagePath} alt={title} />
       </div>
-    )
+    );
   }
 }
 
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,17 +1,25 @@
 // implement MovieList component here
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
-import movies from '../data';
-
 
 class MovieList extends React.Component {
   render() {
-    return(
-      movies.map((movie) => {
-        return <MovieCard movies={movie} key={movie.title}/>
-      })
-    )
+    const { movies } = this.props;
+    return (
+      movies.map((movie) => <MovieCard movie={ movie } key={ movie.title } />)
+    );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  })).isRequired,
+};
 
 export default MovieList;
