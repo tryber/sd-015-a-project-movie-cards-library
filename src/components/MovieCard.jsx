5c65671fd5f96cd1ccd4ea73bbb5c704b9b82c8b
Apolo wilker <wilkerapolocarvalho@gmail.com>
2021-10-06 15:37:53 -0300

    manipulando card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,16 @@
-// implement MovieCard component here
+import React from 'react';
+import data from '..data/';
+
+class MovieCard extends React.Component {
+  render() {
+    const {movies: { title, subtitle, storyline, rating, imagePath}} = this.props;
+    return (
+      <div>
+        <img src={imagePath} alt={title} />
+        <h4> {title} </h4>
+        <h5> {subtitle} </h5>
+        <p>{storyline}</p>
+      </div>
+    );
+  }
+}
