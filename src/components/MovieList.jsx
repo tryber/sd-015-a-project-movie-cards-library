526a147874094283ad1ac53d43b6385613a63f4d
Adadezer <adadezer@gmail.com>
2021-10-06 16:01:06 -0300

    requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    // console.log(movies);
+    return (
+      <div>
+        fsdfsd
+      </div>);
+  }
+}
+
+export default MovieList;
