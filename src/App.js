356a63ed2af1705340495cceb85018cef158edaf
SaraRabelo <rabelorsara@gmail.com>
2021-10-06 19:30:34 -0300

    3 Requisito

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
+import MovieList from './components/MovieList';
+import movies from './data';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <MovieList movie={ movies } />
+
+      </div>
+    );
+  }
 }
 
 export default App;
