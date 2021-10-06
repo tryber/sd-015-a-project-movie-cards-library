3679a64c313176db4e485bec8a9f6e5ef6763f85
arturbacilla <artur.bacilla1@gmail.com>
2021-10-06 20:11:18 -0300

    Applied proptypes on all components and linted everything. Finished Project

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,5 +1,6 @@
 // implement MovieList component here
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
@@ -16,5 +17,9 @@ class MovieList extends React.Component {
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieList;
