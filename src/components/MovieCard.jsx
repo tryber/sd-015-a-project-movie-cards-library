3b4552a5e06f687fdb6b08f8e0fa1f0db5bb3370
Pedro Henrique <pedrohrds05@hotmail.com>
2021-10-06 16:19:48 -0300

    Corrigindo lint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
     const { movie: { imagePath, title, subtitle, storyline } } = this.props;
     return (
-      <div>
+      <div className="movie-card">
         <img src={ imagePath } alt={ title } />
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
@@ -13,5 +14,9 @@ class MovieCard extends React.Component {
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieCard;
