dec17a0c73fb2d2b155c3ef92f0f0aa0e0e4c7b6
Henrique_Veras <theveras1@gmail.com>
2021-10-06 16:29:56 -0300

    correçao do Lint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,28 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    return (
+      <div className="movie-card movie-card-body">
+        <img className="image" src={ movie.imagePath } alt={ movie.title } />
+        <h4 className="movie-card-title">{movie.title}</h4>
+        <h5 className="movie-card-subtitle">{movie.subtitle}</h5>
+        <p className="movie-card-storyline">{movie.storyline}</p>
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    imagePath: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
-// implement MovieList component here
+import React from 'react';
+import movies from '../data';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div key="movieList" className="movie-list">
+        {movies.map((thisMovie) => (
+          <MovieCard key={ thisMovie.title } movie={ thisMovie } />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
