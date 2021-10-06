8ba7042d806239fad448192e9108708262689044
Augusto Alves <augusto.malves@yahoo.com.br>
2021-10-05 21:25:09 -0300

    add e finaliza projeto =D

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,30 @@
-// implement MovieCard component here
+import React from 'react';
+import PropType from 'prop-types';
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
+    return (
+      <div className="movie-card">
+        <img src={ imagePath } alt={ title } />
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+        <Rating rating={ rating } />
+      </div>
+    );
+  }
+}
+MovieCard.propTypes = {
+  movie: PropType.shape({
+    title: PropType.string,
+    subtitle: PropType.string,
+    storyline: PropType.string,
+    imagePath: PropType.string,
+    rating: PropType.number,
+  }).isRequired,
+};
+
+export default MovieCard;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,19 @@
-// implement Rating component here
+import React from 'react';
+import PropType from 'prop-types';
+
+class Rating extends React.Component {
+  render() {
+    const { rating } = this.props;
+    return (
+      <div className="rating">{ rating }</div>
+    );
+  }
+}
+Rating.propTypes = {
+  rating: PropType.number,
+};
+Rating.defaultProps = {
+  rating: 0,
+};
+
+export default Rating;
