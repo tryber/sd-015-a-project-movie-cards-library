d6df76bda753c7b1512d6ba1b4e92e8c44700c28
Emerson Oliveira <emerson.contato@gmail.com>
2021-10-06 19:28:50 -0300

    lint corrigido

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends Component {
   render() {
     const { rating } = this.props;
     return (
-     <p className="rating">{ rating }</p>
+      <p className="rating">{ rating }</p>
     );
   }
 }
+
+Rating.propTypes = {
+  rating: PropTypes.number,
+};
+
+Rating.defaultProps = {
+  rating: 0,
+};
 
 export default Rating;
