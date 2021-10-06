f692d490bf7a1454d0f3c483dd63b3f57a258e5f
Arthur <arthurteixeira0805@gmail.com>
2021-10-06 16:31:15 -0300

    req 3 done

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  // constructor(props) {
+  //   super(props)
+  // }
+
+  render() {
+    return (
+      <MovieCard />
+    );
+  }
+}
+
+export default MovieList;
