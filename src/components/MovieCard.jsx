9448dde5455e81fb239b6e7aab27f9b2c2f0de36
arturbacilla <artur.bacilla1@gmail.com>
2021-10-06 16:45:17 -0300

    Requisites 3 and 4: Basic card and card section structure

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,23 @@
 // implement MovieCard component here
+import React from 'react';
+import movies from '../data';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <section className="card-wrapper">
+        <img className="movie-pic" src="" alt="" />
+        <div className="movie-description">
+          <h2>Titulo</h2>
+          <h4>Subtitulo</h4>
+          <p>Storyline</p>
+        </div>
+        <div className="movie-rating">
+          <p>Rating</p>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default MovieCard;
