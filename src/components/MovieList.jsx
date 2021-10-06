a1823d3b7a2501c6999768489a1b3c76465b3320
Italo Sergio <italosergio@graduate.org>
2021-10-06 16:50:29 -0300

    Requirement 3 and 16

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,25 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <section>
+        <p>oi</p>
+      </section>
+    );
+  }
+}
+
+// MovieList.propTypes = {
+//   data: PropTypes.shape({
+//     Hello: PropTypes.string,
+//   }),
+// };
+
+// MovieList.defaultProps = {
+//   data: {
+//     Hello: '',
+//   },
+// };
+
+export default MovieList;
