fb8a4ff64a6db899c57f0e7536ed888cabbc7fd0
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-10-06 02:56:41 -0300

    requisitos 4 a 16

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
     return (
-      <main className="main">
-        <section className="movieList">
-          <p>Component Here</p>
-        </section>
-      </main>
+      <section className="movie-card">
+        <img className="movie-card-image" src={ movie.imagePath } alt="movie" />
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{ movie.title }</h4>
+          <h5 className="movie-card-subtitle">{ movie.subtitle }</h5>
+          <p className="movie-card-storyline">{ movie.storyline }</p>
+        </div>
+        <Rating rating={ movie.rating }/>
+      </section>
     );
   }
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
+    const cardList = movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />);
     return (
       <main className="main">
-        <section className="movieList">
-          <p>Component Here</p>
+        <section className="movie-list">
+          { cardList }
         </section>
       </main>
     );
