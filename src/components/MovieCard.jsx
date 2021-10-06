f87823501d734e5059b8544af041de0c866b9098
Augusto Alves <augusto.malves@yahoo.com.br>
2021-10-06 18:59:42 -0300

    add style

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,10 +8,12 @@ class MovieCard extends React.Component {
     const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
       <div className="movie-card">
-        <img src={ imagePath } alt={ title } />
-        <h4>{ title }</h4>
-        <h5>{ subtitle }</h5>
-        <p>{ storyline }</p>
+        <div className="movie-card-body">
+          <img src={ imagePath } alt={ title } className="movie-card-image" />
+          <h4 className="movie-card-title">{ title }</h4>
+          <h5 className="movie-card-subtitle">{ subtitle }</h5>
+          <p className="movie-card-storyline">{ storyline }</p>
+        </div>
         <Rating rating={ rating } />
       </div>
     );
