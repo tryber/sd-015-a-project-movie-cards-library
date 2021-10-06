f841397bc40b6c200462a242a7317c2d18739379
Victor Araujo <victor_emanuel9@hotmail.com>
2021-10-06 16:40:34 -0300

    finalizado

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,7 @@
 // implement MovieCard component here
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 export default class MovieCard extends Component {
   render() {
@@ -8,6 +9,10 @@ export default class MovieCard extends Component {
     return (
       <div key={ title } className="movie-card">
         <img src={ imagePath } alt={ subtitle } className="movie-card-image" />
+        <h4 className="movie-card-title">{ title }</h4>
+        <h5 className="movie-card-subtitle">{ subtitle }</h5>
+        <p className="movie-card-storyline">{ storyline }</p>
+        <Rating rating={ rating } />
       </div>);
   }
 }
