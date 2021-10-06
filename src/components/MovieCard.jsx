500976029e3d19446a3498b1912ad002ff21b2ad
João Felipe P <jf.pelliccione@gmail.com>
2021-10-06 14:41:15 -0300

    Conclusão do projeto, adiantado de manhã! :bomb:

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,32 @@
-// implement MovieCard component here
+import React from 'react';
+import propTypes from 'prop-types';
+import Rating from './Rating';
+
+class MovieCard extends React.Component { // Componente React que se transformará em cards de filmes. Será chamado dentro do Componente React de classe 'MovieList'.
+  render() {
+    const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props; // Utilizando object destructuring para captar as informações necessárias para o desenvolvimento de cada card. A props 'movie', desse componente, recebe um objeto que contêm informações de um determinado filme.
+
+    return (
+      <div>
+        <img src={ imagePath } alt={ title } />
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+        <Rating rating={ rating } />
+      </div>
+    );
+  }
+}
+
+// PropTypes:
+MovieCard.propTypes = {
+  movie: propTypes.shape({
+    title: propTypes.string,
+    subtitle: propTypes.string,
+    storyline: propTypes.string,
+    imagePath: propTypes.string,
+    rating: propTypes.number,
+  }).isRequired,
+};
+
+export default MovieCard;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,21 @@
-// implement Rating component here
+import React from 'react';
+import propTypes from 'prop-types';
+
+class Rating extends React.Component { // Componente React que representa o elemento que acumula a nota de cada filme. Será chamado dentro do Componente React de classe 'MovieCard'.
+  render() {
+    const { rating } = this.props; // Utilizando object destructuring para captar a nota de um determinado filme.
+
+    return (
+      <span className="rating">
+        { rating }
+      </span>
+    );
+  }
+}
+
+// PropTypes:
+Rating.propTypes = {
+  rating: propTypes.number.isRequired,
+};
+
+export default Rating;
