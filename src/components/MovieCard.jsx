3c507341fa982585b3e5ed2596b5fc893603bd37
Lucas Loureiro <lucas-loureiro@outlook.com>
2021-10-06 16:20:22 -0300

    requiremoent 6, 7, 8, 9 and 10 done

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
 // import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
-    return <div>teste</div>;
+    const { movie } = this.props;
+    return (
+      <div>
+        <img src={ movie.imagePath } alt="" />
+        <h4>{ movie.title }</h4>
+        <h5>{ movie.subtitle }</h5>
+        <p>{ movie.storyline }</p>
+        <Rating rating={ movie.rating } />
+      </div>
+    );
   }
 }
 
