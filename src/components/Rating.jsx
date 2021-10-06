1a0d5994784a3d5adb184a193c148c427aeb5592
Guarize <pedroguarize@hotmail.com>
2021-10-06 11:21:40 -0300

    Requirement [12]

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
-    return null;
+    const { rating } = this.props;
+
+    return (
+      <div className="movie-card-rating">
+        <div className="rating">
+          { rating }
+        </div>
+      </div>
+    );
   }
 }
+
+Rating.propTypes = {
+  rating: PropTypes.number,
+};
+
+Rating.defaultProps = {
+  rating: 0,
+};
 
 export default Rating;
