05725d0f936729eec1016b9e4ce4b09091bdbe65
rafaelmedeiross <rafael.medeiros@msn.com>
2021-10-06 17:50:55 -0300

    Requisito 3,4,5,6

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,27 @@
-// implement MovieCard component here
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie: { title, subtitle, storyline, imagePath } } = this.props;
+    return (
+      <div>
+        <img src={ imagePath } alt="movie card" />
+        <p>{title}</p>
+        <p>{storyline}</p>
+      </div>
+    );
+  }
+}
+
+// MovieCard.PropTypes = {
+//   movie: PropTypes.shape({
+//     title: PropTypes.string,
+//     subtitle: PropTypes.string,
+//     storyline: PropTypes.string,
+//     rating: PropTypes.number,
+//     imagePath: PropTypes.string,
+//   }),
+// };
+
+export default MovieCard;
