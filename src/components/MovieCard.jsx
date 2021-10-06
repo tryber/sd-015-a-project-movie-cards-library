c5403f4a571b8fe316a2af6dda511d6e83a86a81
Gustavo Lima <lima_gus@outlook.com>
2021-10-06 17:04:58 -0300

    Requisitos 7, 8, 9 e 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
-    return null;
+    //  Essa prop é um objeto contendo as propriedades: title, subtitle, storyline, imagePath e rating.
+    const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
+
+    return (
+      <div>
+        <img src={ imagePath } alt={title} />
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+      </div>
+    );
   }
 }
 
