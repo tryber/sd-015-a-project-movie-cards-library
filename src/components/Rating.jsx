f3fbe8c6fa34b1a4d702b9c99630f0bc695fbf0b
Raphael Taglialegna <raphael.trcosta@gmail.com>
2021-10-06 18:59:16 -0300

    Agora e o ultimo

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,17 +1,18 @@
-// implement Rating component here
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Rating = (props) => {
-  const { rating } = props;
-  return (
-    <div>
-      <p>{ rating }</p>
-    </div>
-  );
-};
+class Rating extends React.Component {
+  render() {
+    const { rating } = this.props;
+    return (
+      <div>
+        <p>{ rating }</p>
+      </div>
+    );
+  }
+}
 
-Rating.proptype = {
+Rating.propTypes = {
   rating: PropTypes.number,
 };
 
