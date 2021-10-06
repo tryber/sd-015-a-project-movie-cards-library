9e35bce9ea2d894f70ba6892d24db3704b49bd33
Apolo wilker <wilkerapolocarvalho@gmail.com>
2021-10-06 15:57:52 -0300

    requisito 3

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import data from '..data/';
 
 class MovieCard extends React.Component {
   render() {
-    const {movies: { title, subtitle, storyline, rating, imagePath}} = this.props;
+    const {movies: { title, subtitle, storyline, imagePath}} = this.props;
     return (
       <div>
         <img src={imagePath} alt={title} />
         <h4> {title} </h4>
         <h5> {subtitle} </h5>
-        <p>{storyline}</p>
+        <p> {storyline} </p>
       </div>
     );
   }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-
+    const { movies } = this.props;
+    return (
+      <div>
+        { movies.map((movie) =>
+        <MovieCard  key={ movie} movies={ movie } />
+        )}
+      </div>
+    );
   }
 }
+
+export default MovieList;
