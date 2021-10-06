c60ffc85265aa76e94d3bc46d101d06a24153c0f
viniciusldn <viniciusldn0@gmail.com>
2021-10-06 15:48:58 -0300

    R3 completed

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,4 +1,9 @@
 // implement MovieList component here
 import React from 'react';
 
-export default props => <section className="movie-list"></section>
+import MovieCard from './MovieCard';
+
+export default props => 
+  <section className="movie-list">
+    <MovieCard />
+  </section>
