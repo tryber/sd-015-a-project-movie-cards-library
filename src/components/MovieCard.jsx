913d2d681d9ca4a6f8158660d0e469329c49c90d
Anderson Alencar <anderson23alencar@gmail.com>
2021-10-06 17:44:42 -0300

    Renderiza imagem do filme

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+
     return (
-      <span>card</span>
+      <img src={ movie.imagePath } alt="" />
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyLine: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieCard;
