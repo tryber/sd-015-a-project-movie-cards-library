431caff17a672f482be54a472ae2049ea3eb1dbe
Tamyres <tamyrescaroline.oliveira@gmail.com>
2021-10-06 15:58:24 -0300

    feat[MovieCard]: add requisitos 7,8,9 e 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,7 +4,12 @@ class MovieCard extends Component {
   render() {
     const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
     return (
-      <section />
+      <section>
+        <img src={imagePath} />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+      </section>
     );
   }
 }
