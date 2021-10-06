9539409151ed4abef96c49fd1d4e79b0b14ff438
gabriel <gabriel.f.takaki@gmail.com>
2021-10-06 17:07:30 -0300

    arrumando lint

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Rating extends Component {
-    render() {
-      const { rating } = this.props;
-      return (  
-      <p className='rating'> { rating }</p>
+  render() {
+    const { rating } = this.props;
+    return (
+      <p className="rating">{ rating }</p>
     );
   }
 }
