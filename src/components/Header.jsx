6a56c1ecade63125c2a42e566046d7f3df32648f
viniciusldn <viniciusldn0@gmail.com>
2021-10-06 15:46:42 -0300

    R1/R2 completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,10 @@
 // implement Header component here
 import React from 'react';
 
-export default props => <header className="movie-card-header"></header>
+export default props => 
+  <header className="movie-card-header">
+    <h1 className="page-title">
+      Movie Cards Library
+    </h1>
+  </header>
+  
