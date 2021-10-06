053bc8277170a1eb8ba94a6aa20569e048449878
Joao-Ortega <joao_ortega17@hotmail.com>
2021-10-06 14:58:36 -0300

    Requisito 03

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        { movies.map((mv, index) => <div key={ index }>{ mv }</div>)}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
