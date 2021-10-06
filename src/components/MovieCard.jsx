1860b5bc0a1afc34ad7e23a47c7dc1d9b1292e79
Emerson Oliveira <emerson.contato@gmail.com>
2021-10-06 19:18:38 -0300

    Projeto Project Movies Card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,20 @@
-// implement MovieCard component here
+import React, { Component } from 'react';
+import Rating from './Rating';
+
+class MovieCard extends Component {
+  render() {
+    const { movie:{ title, subtitle, storyline, imagePath, rating }} = this.props;
+    return (
+      <div key={ title }>
+        <img src={ imagePath } />
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+        <Rating rating={ rating } />
+      </div>
+
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,18 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+
+    return (
+      <div>
+        { movies.map((element) => (
+          <MovieCard key={ element.title } movie={ element } />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
