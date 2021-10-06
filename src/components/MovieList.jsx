c91ef658942696b9b9bfc560a6c91dbf9e4a4e30
djsilva <coxcorp@gmail.com>
2021-10-06 20:57:57 -0300

    atualizaçoes de propTypes

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,19 +1,19 @@
 // implement MovieList component here
 import React from 'react';
 import MovieCard from './MovieCard';
-// import movies from '../data';
+import movies from '../data';
 
 class MovieList extends React.Component {
   render() {
-    const { movies } = this.props;
+    // const { movies } = this.props;
     return (
       movies.map((film) => <MovieCard key={ film.title } movie={ film } />)
     );
   }
 }
 
-MovieList.propTypes = {
-  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
-};
+// MovieList.propTypes = {
+//   movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+// };
 
 export default MovieList;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,5 +1,6 @@
 // implement Rating component here
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
@@ -11,7 +12,11 @@ class Rating extends React.Component {
 }
 
 Rating.propTypes = {
-  rating: PropTypes.number.isRequired,
+  rating: PropTypes.number,
+};
+
+Rating.defaultProps = {
+  rating: 0,
 };
 
 export default Rating;
