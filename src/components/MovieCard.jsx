acca9aaf6ba80d4a731aae134201f524b9d58f8c
Douglas <douglascoelho31@hotmail.com>
2021-10-06 18:37:51 -0300

    alguns requisitos ai

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,14 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
+    return (
+      <div>
+        <img></img>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    // const movies = this.props.movies
+    const { movies } = this.props;
+    return (
+      <div>
+        { movies.map((elemento) => <MovieCard key={ elemento.title } movie={ elemento } />) }
+      </div>
+    );
+  }
+}
+
+export default MovieList;
