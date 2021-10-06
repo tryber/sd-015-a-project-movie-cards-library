24bf90f9de6e818968c4597bdcb684cffc2aa286
Anderson-Zobel <anderson.zobels@gmail.com>
2021-10-06 20:00:00 -0300

    requirement 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,12 +3,13 @@ import React from 'react';
 class MovieCard extends React.Component {
   render() {
     // eslint-disable-next-line react/prop-types
-    const { movie: { imagePath, title, subtitle } } = this.props;
+    const { movie: { imagePath, title, subtitle, storyline } } = this.props;
     return (
       <div className="movie-card">
         <img src={ imagePath } alt="movie-img" />
         <h4 className="movie-card-title">{ title }</h4>
         <h5 className="movie-card-subtitle">{ subtitle }</h5>
+        <p className="movie-card-storyline">{ storyline }</p>
       </div>
     );
   }
