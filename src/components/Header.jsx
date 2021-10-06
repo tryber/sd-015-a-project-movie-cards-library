94b8e2539f3fc7b879cfa758e0cefa0aac523e5f
GTmudin <testederealismo@gmail.com>
2021-10-06 15:58:24 -0300

    requisito 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,2 +1,11 @@
-// implement Header component here
-const r = 'primeiro commit'
+import React from 'react';
+
+class AppHeader extends React.Component {
+    render() {
+        return( <header className = 'movie-card-header'>
+                <h1>Movie Cards Library</h1>
+                </header>
+                )
+    }
+}
+export default AppHeader
