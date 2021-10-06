2a108274b9b2d5c99d117d47c1a1c7f488958784
Diego Brito <diegorbrito9@gmail.com>
2021-10-06 17:07:49 -0300

    consertando erro de linter do rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,6 +5,7 @@ import Rating from './Rating';
 class MovieCard extends React.Component {
   render() {
     const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
+
     return (
       <div className="movie-card movie-card-body">
         <img src={ imagePath } alt={ title } className="movie-card-image" />
