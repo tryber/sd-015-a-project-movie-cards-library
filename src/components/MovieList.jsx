1173b4962902952afe08e62674e9a192866a0213
deobrafantinif <deboraferfan@gmail.com>
2021-10-06 14:59:36 -0300

    requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,16 @@
 // implement MovieList component here
 import React from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    
+    return (
+      <div>
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+      </div>
+    );
   }
 }
 
