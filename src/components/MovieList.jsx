eedcb6a2ec8bf1e3fbac19aad0d86ce56ac613cc
Leandro Hespanhol <leandrohespanhol@gmail.com>
2021-10-06 14:27:42 -0300

    leandro-hespanhol iniciando projeto

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,20 @@
 // implement MovieList component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <main className="movie-list">
+        {(movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />))}
+      </main>);
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.string.isRequired,
+};
+
+export default MovieList;
