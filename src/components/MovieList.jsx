ca66796c46a26a7ca3196f635d3208744ad169c9
Braien <braienmp@outlook.com>
2021-10-06 16:14:43 -0300

    renderiza componentes MovieCard dentro de MovieList e passa um key para cada MovieCard renderizado

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,28 @@
 // implement MovieList component here
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends Component {
   render() {
-    return <section>Teste</section>;
+    const { movies } = this.props;
+    return (
+      <div>
+        {movies.map((movie) => <MovieCard key={ movie.name } />)}
+      </div>);
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      rating: PropTypes.number,
+      imagePath: PropTypes.string,
+    }),
+  ).isRequired,
+};
 
 export default MovieList;
