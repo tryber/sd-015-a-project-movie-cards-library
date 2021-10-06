0f76cf17a837d92ab95fdfa250c13daa94170610
leonardodfelix <leonardo.doria.felix1983@gmail.com>
2021-10-06 07:16:07 -0300

    Requisitos 7 ao 10 feitos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,9 +3,14 @@ import PropTypes from 'prop-types';
 
 export class MovieCard extends Component {
   render() {
+    const { movie: { title, subtitle, storyline, imagePath: src } } = this.props;
+
     return (
-      <div className="movie-card">
-        {null}
+      <div className="movie-card movie-card-body">
+        <img className="movie-card-image" src={ src } alt={ title } />
+        <h4 className="movie-card-title">{ title }</h4>
+        <h5 className="movie-card-subtitle">{ subtitle }</h5>
+        <p className="movie-card-storyline">{ storyline }</p>
       </div>
     );
   }
