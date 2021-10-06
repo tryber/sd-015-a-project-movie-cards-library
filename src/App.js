3bb2044279ca46cc3e64fe4b77055c9038e6fa66
GusMazzinghyDev <gusmazzinghy@gmail.com>
2021-10-06 17:55:51 -0300

    requisito 5

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import Header from './components/Header';
+import MovieList from './components/MovieList';
+
 import './App.css';
+
 
 function App() {
   return (
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
-// implement MovieList component here
+import React from 'react';
+import movies from '../data';
+
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div data-testid="movie-list" className="movie-list">
+        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
+      </div>
+    );
+  }
+}
+export default MovieList;
