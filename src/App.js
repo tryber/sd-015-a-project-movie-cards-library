0f638a9fc830a8cf68213c6ca0a6254b99c9c857
leonardo <leonardo.siepierski@outlook.com>
2021-10-06 19:09:50 -0300

    Teoricamente funciona :D

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-// import MovieCard from './components/MovieCard';
-// import MovieList from './components/MovieList';
+import MovieList from './components/MovieList';
 // import Rating from './components/Rating';
 // import data from './data';
 
@@ -10,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList />
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import data from '../data';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    return (
+      <div className="movie-list">
+        {
+          data.map((element) => <MovieCard movies={ element } key={ element.title } />)
+        }
+      </div>
+    );
   }
 }
 
