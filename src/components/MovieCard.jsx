c8d89d2b039740263be577acc2d92a5d3b9af5ea
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-10-06 01:55:17 -0300

    requisito 2 e 3

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,15 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <main className="main">
+        <section className="movieList">
+          <p>Component Here</p>
+        </section>
+      </main>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <main className="main">
+        <section className="movieList">
+          <p>Component Here</p>
+        </section>
+      </main>
+    );
+  }
+}
+
+export default MovieList;
