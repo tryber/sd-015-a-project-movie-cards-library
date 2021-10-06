a54d9217e25dabb39977ba1178e0889f26a4b82e
Apolo wilker <wilkerapolocarvalho@gmail.com>
2021-10-06 15:01:08 -0300

    primeiro requisito

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,11 @@
-// implement Hellder component here
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <h1> Movie Cards </h1>
+    );
+  }
+}
+
+export default Header;
