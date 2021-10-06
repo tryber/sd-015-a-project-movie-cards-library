db31f361195cc6b68f5d495407233bdd821ff569
Vitor Diorio <vitor.diorio@yahoo.com.br>
2021-10-06 15:15:33 -0300

    Requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
 // implement MovieList component here
+import React from 'react';
+
+export default class MovieList extends React.Component {
+  // eslint-disable-next-line no-useless-constructor
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div />
+    );
+  }
+}
