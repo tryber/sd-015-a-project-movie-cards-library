39dbd50bc9440ed41c52d5df5d49c2a33556e4f2
Lucas Loureiro <lucas-loureiro@outlook.com>
2021-10-06 16:12:00 -0300

    requirements 2, 3, 4 and 5 fixed

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,10 @@
-// implement MovieCard component here
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    return <div>teste</div>;
+  }
+}
+
+export default MovieCard;
