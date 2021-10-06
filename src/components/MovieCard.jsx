09759df3efbbee5c7537e996598076f3ecf77118
Braien <braienmp@outlook.com>
2021-10-06 17:22:14 -0300

    aplica classes para estilizacao

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,11 +7,13 @@ class MovieCard extends React.Component {
   render() {
     const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
     return (
-      <div>
+      <div className="movie-card">
         <img src={ imagePath } alt={ title } />
-        <h4>{ title }</h4>
-        <h5>{ subtitle }</h5>
-        <p>{ storyline }</p>
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{ title }</h4>
+          <h5 className="movie-card-subtitle">{ subtitle }</h5>
+          <p className="movie-card-storyline">{ storyline }</p>
+        </div>
         <Rating rating={ rating } />
       </div>);
   }
