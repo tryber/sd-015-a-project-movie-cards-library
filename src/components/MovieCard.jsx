e104646428f706a1c9650525b377789fcc9e9173
alexandrexpj <alexandrexpjr@hotmail.com>
2021-10-06 14:57:54 -0300

    requisitos 2-5

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,15 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
+    return(
+      <div className="movie-card">
+        
+      </div>
+    )
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+import movies from '../data';
+
+
+class MovieList extends React.Component {
+  render() {
+    return(
+      movies.map((movie) => {
+        return <MovieCard movies={movie} key={movie.title}/>
+      })
+    )
+  }
+}
+
+export default MovieList;
