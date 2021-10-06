0e77ed92abac7c3d2c8a613255bbcf6d1c18ad89
MuriloAmaral <muralha2000@gmail.com>
2021-10-06 15:11:38 -0300

    requisito 3 e 15

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,12 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+// import movies from '../data';
+
+class MovieList extends Component {
+  render() {
+    const movies = this.props;
+
+    return (<section className="movie-list">{movies}</section>);
+  }
+}
+
+export default MovieList;
