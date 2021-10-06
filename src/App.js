5316908119b4797527ee5681c110c5b9533ca473
leonardo <leonardo.siepierski@outlook.com>
2021-10-06 17:46:39 -0300

    Requisitos 1 e 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+// import MovieCard from './components/MovieCard';
+// import MovieList from './components/MovieList';
+// import Rating from './components/Rating';
+// import data from './data';
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
     </div>
   );
 }
