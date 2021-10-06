7e7c34151c91b7521f799d678b25f8b32e5c272a
ricardo-zabir <ricardofonseca.zabir@hotmail.com>
2021-10-06 15:38:43 -0300

    Requisitos 1 ao 7

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,19 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    return (
+      <img src={ movie.imagePath } alt="" />
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    imagePath: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default MovieCard;
