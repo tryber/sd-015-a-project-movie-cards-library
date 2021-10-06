dc6402a83e6548bc5febef7b82a8f919e3389f5c
douglas oliveira <douglasgama0@gmail.com>
2021-10-06 18:16:23 -0300

    adicionando elementos aos MovieCard, requisitos 7, 8, 9

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import movies from '../data';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
     return (
-      <section className="movie-list" />
+      <section className="movie-list">
+        { movies.map((movie, index) => (
+          <MovieCard key={ `Movie Title ${index}` } movie={ movie } />)) }
+      </section>
     );
   }
 }
