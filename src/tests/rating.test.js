2f8aa48c3f6ab01621d4a221e7df7ab50fa5a4c9
William Schmidt <9williamschmidt@gmail.com>
2021-10-07 19:57:37 -0300

    lint

--- src/tests/rating.test.js
+++ src/tests/rating.test.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { mount, shallow } from 'enzyme';
 import MovieCard from '../components/MovieCard';
 import Rating from '../components/Rating';
