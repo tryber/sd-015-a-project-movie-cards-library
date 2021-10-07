fc7d6753c5e3a0d434f4aaefb878e97fabfc0269
Alexsandro Xavier <alexsandro060299@outlook.com>
2021-10-07 09:55:20 -0300

    Termina criaçao de Movie List

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,19 @@
 // implement MovieList component here
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends Component {
   render() {
-    return <div />;
+    // const {  } = movies;
+    const { movies } = this.props;
+    return movies.map(({ title }, movie) => (
+      <MovieCard key={ title } movie={ movie } />
+    ));
   }
 }
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieList;
