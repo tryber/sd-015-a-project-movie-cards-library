9712599cdbe9d275e45e1f95a2081404f9325795
renatozr <renatozr07@gmail.com>
2021-10-07 09:39:36 -0300

    requisito 6,7,8,9

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,7 +6,15 @@ class MovieCard extends React.Component {
     const { movie } = this.props;
     return (
       <div className="movie-card">
-        
+        <img
+          className="movie-card-image"
+          src={ movie.imagePath }
+          alt={ `Imagem do filme ${movie.title}` }
+        />
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{ movie.title }</h4>
+          <h5 className="movie-card-subtitle">{ movie.subtitle }</h5>
+        </div>
       </div>
     );
   }
