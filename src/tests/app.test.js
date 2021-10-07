40fdee6855dbac6c36b9a52f0794c168784cfbd6
herzogcassia <cassiahherzog@gmail.com>
2021-10-07 00:46:55 -0300

    primeiro commit

--- src/tests/app.test.js
+++ src/tests/app.test.js
@@ -8,6 +8,7 @@ describe('15 - Crie um componente `<App />`', () => {
   it('Renderize `<Header />` dentro do componente `<App />`', () => {
     wrapper = shallow(<App />);
     expect(wrapper.find('Header').length).toEqual(1);
+  
   });
 });
 
