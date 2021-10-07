7bffb192583a6ac16ff1e2026c6aa022b0b8f687
SaraRabelo <rabelorsara@gmail.com>
2021-10-06 23:26:04 -0300

    Verificando Problema

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,20 +4,27 @@ import PropTypes from 'prop-types';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath } = movie;
     // const { key } = this.props;
     return (
       <div>
-        <h4>{movie.title}</h4>
-        <h5>{movie.subtitle}</h5>
-        <p>{movie.storyline}</p>
-        <img src={ movie.imagePath } alt=" " />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+        <img src={ imagePath } alt=" " />
       </div>
     );
   }
 }
 
 MovieCard.propTypes = {
-  movie: PropTypes(PropTypes.object).isRequired,
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
 };
 
 export default MovieCard;
