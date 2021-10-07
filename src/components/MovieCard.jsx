7f0b8abc2f02c64f65851049f18535602311870e
André <andre.bonaf@gmail.com>
2021-10-06 22:38:09 -0300

    Requisito 3 e 4

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,38 @@
 // implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movies } = this.props;
+    const { title, subtitle, storyline, imagePath } = movies;
+    return (
+      <div className="MovieCard">
+        <img alt="Imagem do filme" src={ imagePath } />
+        <h2>{title}</h2>
+        <h3>{subtitle}</h3>
+        <p>{storyline}</p>
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movies: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+  }),
+};
+
+MovieCard.defaultProps = {
+  movies: {
+    title: 'vazio',
+    subtitle: 'vazio',
+    storyline: 'vazio',
+    imagePath: '',
+  },
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,18 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+import data from '../data';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div>
+        {
+          data.map((movies) => <MovieCard movies={ movies } key={ movies.title } />)
+        }
+      </div>
+    );
+  }
+}
+
+export default MovieList;
