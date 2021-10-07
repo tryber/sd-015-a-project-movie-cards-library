0bbf74598fb0d458d4710a47eea9cddc06e49f4f
Matheus Muniz <matheusmuniz100@hotmail.com>
2021-10-07 18:14:08 -0300

    requisitos 12 e 13

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,12 +1,21 @@
 // implement Rating component here
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends Component {
   render() {
+      const { nota } = this.props;
+
     return (
-      <div />
+      <div>
+          <span className="movie-card-rating">{ nota }</span>
+      </div>
     );
   }
 }
+
+Rating.propTypes = {
+    rating: PropTypes.number.isRequired,
+};
 
 export default Rating;
