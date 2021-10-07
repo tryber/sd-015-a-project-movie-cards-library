e28dcca6cbf5894c789e3f67b77afb5d487fb0fc
thomazj <thomaz.jefferson01@protonmail.com>
2021-10-06 22:59:20 -0300

    todos requisitos restantes mais reestruturacao e lintfix

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,18 +1,27 @@
 // implement MovieList component here
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    // vem do App requisito pedia pra usar como props
     const { movies } = this.props;
     return (
-      <div movies = { movies }> 
+      // passar movies para poder usar dentro da div e fazer o map.
+      <div movies={ movies }>
         {
+          // passando movie como props para o card
           movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)
         }
       </div>
-      
     );
   }
 }
+
+// validar se movies está passando um array de objetos
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 export default MovieList;
