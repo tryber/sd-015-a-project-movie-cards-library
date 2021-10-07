50a5c1975be2c9a08834b773c61845ce3491de72
Rogério Maia Jr. [Roger] <rogeriomaiajr13@gmail.com>
2021-10-07 04:41:15 -0300

    Css classnames adjustments

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,11 +7,11 @@ class MovieCard extends Component {
     const { movie } = this.props;
     const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
-      <div>
+      <div className="movie-card">
         <img src={ imagePath } alt={ title } />
-        <h4>{ title }</h4>
-        <h5>{ subtitle }</h5>
-        <p>{ storyline }</p>
+        <h4 className="movie-card-title">{ title }</h4>
+        <h5 className="movie-card-subtitle">{ subtitle }</h5>
+        <p className="movie-card-storyline">{ storyline }</p>
         <Rating rating={ rating } />
       </div>
     );
