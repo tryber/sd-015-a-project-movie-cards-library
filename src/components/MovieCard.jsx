1b3f348225299cf4c705c3198356b888de39acdf
Marcelo Adriano <marcelo.gomes.adriano@gmail.com>
2021-10-06 23:11:57 -0300

    Estilizando

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,17 +2,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Rating from './Rating';
+import '../App.css';
 
 class MovieCard extends React.Component {
   render() {
     const { movie: { imagePath, title, subtitle, rating, storyline } } = this.props;
     return (
-      <div>
-        <img src={ imagePath } alt="qualquerCoisa" />
-        <div>
-          <h4>{ title }</h4>
-          <h5>{ subtitle }</h5>
-          <p>{ storyline }</p>
+      <div className="movie-card">
+        <img className="movie-card-image" src={ imagePath } alt="qualquerCoisa" />
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{ title }</h4>
+          <h5 className="movie-card-subtitle">{ subtitle }</h5>
+          <p className="movie-card-storyline">{ storyline }</p>
         </div>
         <Rating rating={ rating } />
       </div>
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,13 +1,13 @@
 // implement Rating component here
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../App.css';
 
 class Rating extends React.Component {
   render() {
     const { rating } = this.props;
     return (
-      <div>
-        <span>Rating</span>
+      <div className="movie-card-rating">
         <span className="rating">{ rating }</span>
       </div>
     );
