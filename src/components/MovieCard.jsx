14028216f0f8aa54b9121af8e0db922476391ac0
Matheus Burthon <matatiaspatrizio@hotmail.com>
2021-10-07 11:05:56 -0300

    adicionando mais estilos na pagina.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,13 +7,9 @@ class MovieCard extends Component {
     const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
     return (
       <div key={ title }>
-        <div>
-          <img src={ imagePath } alt={ subtitle } />
-        </div>
-        <div>
-          <h4 className="movie-card-title">{title}</h4>
-          <h5 className="movie-card-subtitle">{subtitle}</h5>
-        </div>
+        <img src={ imagePath } alt={ subtitle } className=".movie-card-image" />
+        <h4 className="movie-card-title">{title}</h4>
+        <h5 className="movie-card-subtitle">{subtitle}</h5>
         <p className="movie-card-storyline">{storyline}</p>
         <Rating rating={ rating } />
       </div>
