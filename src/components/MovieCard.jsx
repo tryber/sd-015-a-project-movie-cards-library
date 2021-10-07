c4736e6d0a900007aa275ea785e83cb27755d1d4
SaraRabelo <rabelorsara@gmail.com>
2021-10-07 00:38:51 -0300

    Requisitos 11 e 13 - corrigindo Lint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,7 +5,7 @@ import Rating from './Rating';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, storyline, rating, imagePath } = movie;
+    const { title, subtitle, storyline, imagePath } = movie;
     // const { key } = this.props;
     return (
       <div>
