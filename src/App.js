fa615f3634d73c579e4b0f0cb9bdd0ab0ebb732d
gabriel <gabriel.f.takaki@gmail.com>
2021-10-07 14:12:09 -0300

    comentando o codigo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
+import movies from './data';
+// importa o React, o header, movieList e o Json movies
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList movies={ movies } />
     </div>
+    // cria uma div pra ser pai dos demais elementos (pois tem mais de um )
+    // chama o arquivo header que contem o cabeçalho do projeto.
+    // chama a class MovieList e atribui o valor de movies que contem na sua composição o array movies do arquvido data.js (agora o codigo roda usando esses dados como parametro)
   );
 }
 
 export default App;
+// exporta a function app pro arquvio index.js (main)
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Rating from './Rating';
+// importar React ProptYpes e o arquivo rating
 
 class MovieCard extends Component {
   render() {
     const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
+    // reestruturando a constante Movie para pegar as propriedades a cima de quando um valor for atribuido a ela (no caso o array movies data.js)
     return (
       <section>
         <img src={ imagePath } alt="" />
@@ -13,6 +15,7 @@ class MovieCard extends Component {
         <p>{ storyline }</p>
         <Rating rating={ rating } />
       </section>
+    // cria uma secton que vai ser usada como formato padrão para todos os cards com as propriedades de cada filme;
     );
   }
 }
@@ -28,5 +31,7 @@ MovieCard.propTypes = {
     },
   ).isRequired,
 };
+// requer que as propriedades da const movie sejam do typeOf selecionado ex: string, number, booelan etc...
 
 export default MovieCard;
+// exporta o arquivo que vai ser usado em "MovieList.jsx"
