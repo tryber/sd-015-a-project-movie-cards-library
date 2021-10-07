8f7bba0fd876d956a0d52e182498d8746317dc1b
renatozr <renatozr07@gmail.com>
2021-10-07 09:21:47 -0300

    requisito 3,4,5

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,8 +1,15 @@
 // implement MovieCard component here
-// import React from 'react';
+import React from 'react';
 
-// class MovieCard extends React.Component {
-//   render() {
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    return (
+      <div className="movie-card">
+        
+      </div>
+    );
+  }
+}
 
-//   }
-// }
+export default MovieCard;
