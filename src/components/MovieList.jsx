813f7ecb8f38c1007d925af2c3dea7831a3f2d24
Alexsandro Xavier <alexsandro060299@outlook.com>
2021-10-07 09:39:39 -0300

    Cria components basicos com exportaçoes e importaçoes necessarias

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,10 @@
 // implement MovieList component here
+import React, { Component } from 'react';
+
+class MovieList extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default MovieList;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,10 @@
 // implement Rating component here
+import React, { Component } from 'react';
+
+class Rating extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default Rating;
