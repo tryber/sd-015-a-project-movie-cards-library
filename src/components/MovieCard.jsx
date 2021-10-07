3d42e6204d837c14be33cd077c6991082f58206c
MoisésFigueira <moisesmichael@gmail.com>
2021-10-06 21:55:33 -0300

    exercise 7

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,7 +4,20 @@ import PropTypes from 'prop-types';
 class MovieCard extends React.Component {
   render() {
     const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
+    return (
+      <div>
+        <img src={ imagePath } alt="img" />
+      </div>
+    )
   }
 }
-
+MovieCard.propTypes = {
+  movie: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+    rating: PropTypes.number,
+  })).isRequired,
+};
 export default MovieCard;
