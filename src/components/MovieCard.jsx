94dff59f969824778dd47d440265e6d67ee3ce17
Marcelo Adriano <marcelo.gomes.adriano@gmail.com>
2021-10-06 22:10:36 -0300

    Requisito 17 e Lint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,27 +1,33 @@
 // implement MovieCard component here
 import React from 'react';
+import PropTypes from 'prop-types';
 import Rating from './Rating';
 
-// title: 'Kingsglaive',
-// subtitle: 'Final Fantasy XV',
-// storyline: 'King Regis, who oversees the land of Lucis, commands his army of soldiers to protect the kingdom from the Niflheim empire\'s plans to steal the sacred crystal.',
-// rating: 4.5,
-// imagePath: 'images/Kingsglaive_Final_Fantasy_XV.jpg',
-
 class MovieCard extends React.Component {
-    render() {
-        return(
-            <div>
-                <img src={this.props.movie.imagePath} alt='qualquerCoisa' />
-                <div>
-                    <h4>{this.props.movie.title}</h4>
-                    <h5>{this.props.movie.subtitle}</h5>
-                    <p>{this.props.movie.storyline}</p>
-                </div>
-                <Rating rating={this.props.movie.rating} />
-            </div>
-        );
-    }
+  render() {
+    const { movie: { imagePath, title, subtitle, rating, storyline } } = this.props;
+    return (
+      <div>
+        <img src={ imagePath } alt="qualquerCoisa" />
+        <div>
+          <h4>{ title }</h4>
+          <h5>{ subtitle }</h5>
+          <p>{ storyline }</p>
+        </div>
+        <Rating rating={ rating } />
+      </div>
+    );
+  }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.exact({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,15 +1,23 @@
 // implement MovieList component here
 import React from 'react';
-import MovieCard from './MovieCard.jsx';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
-    render() {
-        return(
-            <div>
-                {this.props.movies.map((movie, index) => <MovieCard key={`Movie Title ${1 + index}`} movie={movie} />)}
-            </div>
-        );
-    }
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        { movies.map((movie, index) => (
+          <MovieCard key={ `Movie Title ${1 + index}` } movie={ movie } />
+        )) }
+      </div>
+    );
+  }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieList;
