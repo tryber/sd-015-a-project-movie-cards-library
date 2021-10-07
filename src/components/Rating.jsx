73f2403d6b2a71dc884d4bc4013acfde59c73b60
Alexsandro Xavier <alexsandro060299@outlook.com>
2021-10-07 10:50:44 -0300

    Termina criaçao de rating

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,10 +1,21 @@
 // implement Rating component here
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends Component {
   render() {
-    return <div />;
+    const { rating } = this.props;
+    return (
+      <p className="rating">
+        {rating}
+      </p>);
   }
 }
 
+Rating.propTypes = {
+  rating: PropTypes.number,
+};
+Rating.defaultProps = {
+  rating: 0,
+};
 export default Rating;
