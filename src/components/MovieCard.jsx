c4a53efce5a82689d4ad8cf6e76393da71b19dc8
renatozr <renatozr07@gmail.com>
2021-10-07 10:33:24 -0300

    requisito 17 e adiciona image default

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,26 +1,47 @@
 // implement MovieCard component here
 import React from 'react';
+import PropTypes from 'prop-types';
 import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    const { movie } = this.props;
+    const { movie: { imagePath, title, subtitle, storyline, rating } } = this.props;
     return (
       <div className="movie-card">
         <img
           className="movie-card-image"
-          src={ movie.imagePath }
-          alt={ `Imagem do filme ${movie.title}` }
+          src={ imagePath }
+          alt={ `Imagem do filme ${title}` }
         />
         <div className="movie-card-body">
-          <h4 className="movie-card-title">{ movie.title }</h4>
-          <h5 className="movie-card-subtitle">{ movie.subtitle }</h5>
-          <p className="movie-card-storyline">{ movie.storyline }</p>
+          <h4 className="movie-card-title">{ title }</h4>
+          <h5 className="movie-card-subtitle">{ subtitle }</h5>
+          <p className="movie-card-storyline">{ storyline }</p>
         </div>
-        <Rating rating={ movie.rating } />
+        <Rating rating={ rating } />
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }),
+};
+
+MovieCard.defaultProps = {
+  movie: {
+    title: 'Título',
+    subtitle: 'Subtítulo',
+    storyline: 'Sinopse',
+    rating: 0,
+    imagePath: 'images/imagem-default.png',
+  },
+};
 
 export default MovieCard;
