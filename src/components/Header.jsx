aa8f3c5b49896335a5718895ebbfd1b9739fd2aa
Gabriel-Araujo11 <gabriel_arsi@hotmail.com>
2021-10-07 12:51:10 -0300

    Requisito 01 e 02

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,13 @@
-// const Header = (Header) => {
-//     return
-// };
+import React from 'react';
 
-// export default Header
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
