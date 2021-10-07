32bb9aa11d06eb4b9536e00f7a624adf2439148b
Renato Moreno <renats.mc@gmail.com>
2021-10-06 22:58:37 -0300

    requisito 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -11,7 +11,6 @@ class MovieCard extends React.Component {
           <h4 className="movie-card-title">{ title }</h4>
           <h5 className="movie-card-subtitle">{ subtitle }</h5>
           <p className="movie-card-storyline">{ storyline }</p>
-          <p className="rating">{ rating }</p>
         </section>
       </section>
     );
