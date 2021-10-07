1c19575650190763b105d69b4b207806857a5b53
Anderson-Zobel <anderson.zobels@gmail.com>
2021-10-06 22:02:51 -0300

    requirement 17

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    // eslint-disable-next-line react/prop-types
     const { movie: { imagePath, title, subtitle, storyline, rating } } = this.props;
     return (
       <div className="movie-card">
@@ -11,10 +11,20 @@ class MovieCard extends React.Component {
         <h4 className="movie-card-title">{ title }</h4>
         <h5 className="movie-card-subtitle">{ subtitle }</h5>
         <p className="movie-card-storyline">{ storyline }</p>
-        <Rating rating= { rating } />
+        <Rating rating={ rating } />
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -13,7 +13,7 @@ class MovieList extends React.Component {
   }
 }
 MovieList.propTypes = {
-  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+  movies: PropTypes.arrayOf(PropTypes.shape).isRequired,
 };
 
 export default MovieList;
