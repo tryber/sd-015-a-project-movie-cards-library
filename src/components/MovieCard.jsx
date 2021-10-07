622c40a0d46cfb50461be2581e15c5b826fe2c8b
MoisésFigueira <moisesmichael@gmail.com>
2021-10-06 21:57:41 -0300

    exercises 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -9,6 +9,7 @@ class MovieCard extends React.Component {
         <img src={ imagePath } alt="img" />
         <h4>{ title }</h4>
         <h5>{ subtitle}</h5>
+        <p>{ storyline}</p>
       </div>
     )
   }
