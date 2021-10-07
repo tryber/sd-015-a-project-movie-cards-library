a629ea05e1b144a8d9aebc68acb800d269cbc16f
Matheus Burthon <matatiaspatrizio@hotmail.com>
2021-10-06 22:43:10 -0300

    feito o requisito 11.

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
-export default class MovieList extends React.Component {
+export default class MovieList extends Component {
   render() {
     const { movies } = this.props;
     return (
