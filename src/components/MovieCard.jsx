485529666111ddb43a901c4ae6bc5caf0e39c2aa
Matheus Burthon <matatiaspatrizio@hotmail.com>
2021-10-07 11:23:48 -0300

    colocando mais css no React.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,14 +4,18 @@ import Rating from './Rating';
 
 class MovieCard extends Component {
   render() {
-    const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
+    const {
+      movie: { title, subtitle, storyline, rating, imagePath },
+    } = this.props;
     return (
-      <div key={ title }>
-        <img src={ imagePath } alt={ subtitle } className=".movie-card-image" />
-        <h4 className="movie-card-title">{title}</h4>
-        <h5 className="movie-card-subtitle">{subtitle}</h5>
-        <p className="movie-card-storyline">{storyline}</p>
-        <Rating rating={ rating } />
+      <div key={ title } className="movie-list">
+        <div className="movie-card">
+          <img src={ imagePath } alt={ subtitle } className=".movie-card-image" />
+          <h4 className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
+          <Rating rating={ rating } />
+        </div>
       </div>
     );
   }
