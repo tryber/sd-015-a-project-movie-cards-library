2a520c5d736392516c846b8b359d388f516b6563
Matheus Burthon <matatiaspatrizio@hotmail.com>
2021-10-06 21:41:27 -0300

    feito mais uma correçao de tag.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 class MovieCard extends Component {
   render() {
-    const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
+    const { movie: { title, subtitle, storyline, imagePath } } = this.props;
     return (
       <div key={ title }>
         <div>
@@ -12,11 +12,8 @@ class MovieCard extends Component {
         <div>
           <h4>{title}</h4>
           <h5>{subtitle}</h5>
-          <p>{storyline}</p>
         </div>
-        <div>
-          <p>{rating}</p>
-        </div>
+        <p>{storyline}</p>
       </div>
     );
   }
