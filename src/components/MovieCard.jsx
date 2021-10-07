51726d40bdad443eb2c9e599f90ccacaad231997
renatozr <renatozr07@gmail.com>
2021-10-07 09:54:42 -0300

    requisito 10,11,12,13,14,15,16

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 // implement MovieCard component here
 import React from 'react';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
@@ -14,7 +15,9 @@ class MovieCard extends React.Component {
         <div className="movie-card-body">
           <h4 className="movie-card-title">{ movie.title }</h4>
           <h5 className="movie-card-subtitle">{ movie.subtitle }</h5>
+          <p className="movie-card-storyline">{ movie.storyline }</p>
         </div>
+        <Rating rating={ movie.rating } />
       </div>
     );
   }
