699e61e473e90e869360c0cc132e483fc16f139c
Emily Carvalho <nilsen.loli.emily@gmail.com>
2021-10-07 13:53:22 -0300

    requisito 10 e lint

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-import MovieList from './components/MovieList'
+import MovieList from './components/MovieList';
 import movie from './data';
 
 function App() {
