77f4422e62e6db2cf42c8f9becc31682dadcdf86
Renato Adorno <renatowillian.2000@gmail.com>
2021-10-08 19:05:29 -0300

    requisito 11 ao 16

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -6,7 +6,7 @@ class MovieList extends Component {
   render() {
     const { movies } = this.props;
     return (
-      <div className="pokedex">
+      <div>
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
     );
