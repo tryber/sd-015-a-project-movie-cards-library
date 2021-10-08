ec3023bd9df03d8395d0c26bdb9590a86c7fc6ba
Eliana Nunes <elianadnunes@gmail.com>
2021-10-08 17:31:09 -0300

    Requisito 6 cria um componente MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,12 @@
-// implement MovieCard component here
+import React from "react";
+
+
+class MovieCard extends React.Component {
+    render() {
+        return(
+            <p></p>
+        );
+    }
+}
+
+export default MovieCard;
