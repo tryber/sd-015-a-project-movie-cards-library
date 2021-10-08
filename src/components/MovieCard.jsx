2e9db67e0b81d38a5ba96bcbaff2de42a8f41b17
Eliana Nunes <elianadnunes@gmail.com>
2021-10-08 19:45:05 -0300

    Requisitos 11 e 12

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,18 +1,17 @@
-import React from "react";
-
+import React from 'react';
 
 class MovieCard extends React.Component {
-    render() {
-        const {movie: { title, subtitle, storyline, rating, imagePath }} = this.props;
-        return(
-            <div>
-                <img src={imagePath}/>
-                <h4>{title}</h4>
-                <h5>{subtitle}</h5>
-                <p>{storyline}</p>
-            </div>
-        );
-    }
+  render() {
+    const { movie: { title, subtitle, storyline, imagePath } } = this.props;
+    return (
+      <div>
+        <img src={ imagePath } />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+      </div>
+    );
+  }
 }
 
 export default MovieCard;
