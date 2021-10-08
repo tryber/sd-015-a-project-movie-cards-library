d935059a349e5c01d76d15d05c3484b680d1754f
Lucas Macedo <lucasrrm22@gmail.com>
2021-10-08 08:05:13 -0300

    requisito 8

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -9,8 +9,8 @@ class MovieCard extends React.Component {
     return (
       <div key={ title } className="movie-card">
         <img src={ imagePath } alt="movie" className="movie-card-image" />
-        <p className="movie-card-title"><h4>{ title }</h4></p>
-        <p className="movie-card-subtitle">{ subtitle }</p>
+        <h4 className="movie-card-title">{ title }</h4>
+        <h5 className="movie-card-subtitle">{ subtitle }</h5>
         <p className="movie-card-storyline">{ storyline }</p>
       </div>
     );
