d81f2ed8dcde5cbc8c74bffad36a4658cafa3c2b
GTmudin <testederealismo@gmail.com>
2021-10-07 22:34:35 -0300

    correçao sintaxe

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import { render } from 'enzyme';
 import './App.css';
 // capturo meu componente Header.
 import Header from './components/Header';
 // capturo meu objeto que está contido em data.js para ser usado como valor do meu props(argumento).
-import Data from './data';
 import MovieList from './components/MovieList';
-import { render } from 'enzyme';
+import Movies from './data';
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
       {/* capturo a minha tag header com import e renderizo o que é feito no
       componente/header. */}
       <Header />
-      <MovieList movies={ Data } />
+      <MovieList Movies={ Movies } />
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -24,6 +24,7 @@ class MovieList extends React.Component {
 }
 
 MovieList.propTypes = {
-  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+  Movies: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
+
 export default MovieList;
