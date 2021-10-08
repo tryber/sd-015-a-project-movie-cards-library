3bda89ea9713e65ff3963708eca81a7627b450b3
Lucas Ferreira <lucas6rodrigues@yahoo.com.br>
2021-10-07 22:41:15 -0300

    testes para saber qual requisito passou

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import Header from './Header'
+import Header from './components/Header';
+import MovieList from './components/MovieList';
 import './App.css';
+import MovieCard from './components/MovieCard';
+import movie from './data';
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
+    <div>
+      <Header />
+      <MovieList />
+      {movie.map((item, index) => (
+        <MovieCard
+          key={ index }
+          title={ item.title }
+          subtitle={ item.subtitle }
+          storyline={ item.storyline }
+        />
+      ))}
     </div>
   );
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,18 @@
-// implement MovieCard component here
+import React from 'react';
+
+import movies from '../data';
+
+const { title, subtitle, storyline } = movies;
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>{title}</h3>
+        <p>{subtitle}</p>
+        <p>{storyline}</p>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
