de625d8f623b2fc5b166a4ef2af8610eeae3cb86
herzogcassia <cassiahherzog@gmail.com>
2021-10-08 19:42:10 -0300

    bomba 5

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,18 @@
 // implement MovieCard component here
+import React from 'react';
+// import { movies } from './data';
+// import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    return (
+      <header>
+        <div> { movie.title } </div>
+        <div> { movie.subtitle } </div>
+      </header>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,22 @@
 // implement MovieList component here
-import React from 'react';
+import React from "react";
+import MovieCard from "./MovieCard";
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
-      <div className="movies" />
+      <>
+        <header>
+          <h1>Movie Cards Library</h1>
+        </header>
+        <div className="movie-list">
+          {movies.map(({ title }, movie) => (
+            <MovieCard key={ title } movie={ movie } />
+          ))}
+          ;
+        </div>
+      </>
     );
   }
 }
