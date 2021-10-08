6cb9031acb721ca99a3ee4a5550291c63463dcc8
uauditore <fauditore2912@gmail.com>
2021-10-07 21:21:41 -0300

    quebrando muito a cabeça

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,23 @@
-// implement MovieCard component here
+import React from 'react';
+import Proptypes from 'prop-types';
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+  render() {
+    // eslint-disable-next-line react/prop-types
+    const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
+    return (
+      <div className="movie-card">
+        <img src={ imagePath } alt="imagem do Game" />
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{ title }</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
+        </div>
+        <div className="rating">
+          <Rating rating={ rating } />
+        </div>
+      </div>
+    );
+  }
+}
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
-// implement MovieList component here
+import React from 'react';
+import Proptypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render(){
+    
+    const { movies } = this.props;
+    return (
+      <div>
+        {
+          
+        }
+      </div>
+    )
+  }
+}
