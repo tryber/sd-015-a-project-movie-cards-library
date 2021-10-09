872d7e229a3c2a64ebd42e3df29d6abf6da814f6
Gabriel Moda <gabriel-moda@hotmail.com.br>
2021-10-09 20:21:36 -0300

    Eslint... agora vai, falta CSS

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,18 +1,18 @@
 // implement MovieCard component here
 import React from 'react';
-import Rating from './Rating';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 export default class MovieCard extends React.Component {
   render() {
     const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
     return (
       <div>
-        <img src={imagePath} alt="img movie" />
+        <img src={ imagePath } alt="img movie" />
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
-        <Rating rating={rating} />
+        <Rating rating={ rating } />
       </div>
     );
   }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,14 +1,14 @@
 // implement MovieList component here
 import React from 'react';
-import MovieCard from './MovieCard';
 import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 export default class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
       <div>
-        {movies.map((element) => <MovieCard key={element.title} movie={element} />)}
+        {movies.map((element) => <MovieCard key={ element.title } movie={ element } />)}
       </div>
     );
   }
