0a146d0af619059544949900c42085e75c3a1dfc
Eliana Nunes <elianadnunes@gmail.com>
2021-10-09 08:31:51 -0300

    Requisito 4 e 5

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
       <div className="movie-list">
-        {this.props.movies}
+        {movies.map((element) => <MovieCard movie={ element } key={ element.title } />)}
       </div>
     );
   }
