52004d92d77c764597cbe38d6045ca5e78daf58f
uauditore <fauditore2912@gmail.com>
2021-10-08 22:25:57 -0300

    indo dormir

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,15 +3,23 @@ import Proptypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
-  render(){
-    
+  render() {
     const { movies } = this.props;
     return (
       <div>
         {
-          
+          // eslint-disable-next-line react/prop-types
+          movies.map((movie) => <MovieCard movie={ movie } key={ movie.title } />)
         }
       </div>
-    )
+    );
   }
 }
+MovieList.propTypes = {
+  movies: Proptypes.arrayOf(Proptypes.object),
+};
+
+MovieList.defaultProps = {
+  movies: [{}],
+};
+export default MovieList;
