46f71cee10251b487aa1e379d104a1059d99965c
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2021-10-09 02:41:29 -0300

    adequa lint, agora sim acabou

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,14 +8,18 @@ class MovieCard extends React.Component {
 
     return (
       <div className="movie-card">
-        <img className="movie-card-image" src={ imagePath } alt={ `poster do filme ${title}` } />
+        <img
+          className="movie-card-image"
+          src={ imagePath }
+          alt={ `poster do filme ${title}` }
+        />
         <div className="movie-card-body">
           <h4 className="movie-card-title">{ title }</h4>
           <h5 className="movie-card-subtitle">{ subtitle }</h5>
           <p className="movie-card-storyline">{ storyline }</p>
         </div>
         <div className="movie-card-rating">
-            <Rating rating={ rating } />
+          <Rating rating={ rating } />
         </div>
       </div>
     );
