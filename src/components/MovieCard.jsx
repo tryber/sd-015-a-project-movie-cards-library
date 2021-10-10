132818362ac412dabec665e24f07252df3609dcd
Junio <junioalves2005@hotmail.com>
2021-10-10 16:14:50 -0300

    ajuste no eslint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,11 +8,11 @@ class MovieCard extends React.Component {
     const { movie } = this.props;
     const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
-      <div className="movie-card" key={title}>
-        <img src={imagePath} alt=" " className="movie-card-image" />
-        <h4 className="movie-card-title">{title}</h4>
-        <h5 className="movie-card-subtitle">{subtitle}</h5>
-        <p className="movie-card-storyline">{storyline}</p>
+      <div className="movie-card" key={ title }>
+        <img src={ imagePath } alt=" " className="movie-card-image" />
+        <h4 className="movie-card-title">{ title }</h4>
+        <h5 className="movie-card-subtitle">{ subtitle }</h5>
+        <p className="movie-card-storyline">{ storyline }</p>
         <Rating rating={rating} />
       </div>
     );
