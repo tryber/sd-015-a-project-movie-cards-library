e4a35dab40d590685d78225723cfc68e56ac7f0e
Marcelo Alessandro <marceloouropreto@gmail.com>
2021-10-10 18:34:48 -0300

    Corrige lint

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -9,7 +9,7 @@ class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
-      <div className="movie-list"> 
+      <div className="movie-list">
         { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div> // key para cada Moviecard utilizando map
     );
