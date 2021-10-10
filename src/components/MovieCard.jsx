bd15ffe2632f03a357a71ddb72a0db906387a4c1
carla <carlabazevedoa@gmail.com>
2021-10-10 13:15:29 -0300

    quesito 1 ao 11 feito.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,30 @@
 // implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { key, movie } = this.props;
+    return (
+      <div>
+        <img src={ movie.imagePath } alt={ key } />
+        <h4>{movie.title}</h4>
+        <h5>{movie.subtitle}</h5>
+        <p>{movie.storyline}</p>
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  key: PropTypes.string.isRequired,
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
+
+export default MovieCard;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,15 @@
 // implement Rating component here
+// implement MovieCard component here
+import React from 'react';
+// import ReactDOM from 'react-dom';
+// import data from '../data';
+
+class Rating extends React.Component {
+  render() {
+    return (
+      <p> </p>
+    );
+  }
+}
+
+export default Rating;
