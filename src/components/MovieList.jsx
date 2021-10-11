78c7e2b413b4b5159dd047bfe9a1b94e4293af3a
Cla Dávila <clarice.coliveira@yahoo.com.br>
2021-10-11 17:09:01 -0300

    Requisitos 2 - Renderize um texto no <Header /> e 3 - Crie um componente <MovieList />

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Movie List</h1>
+      </div>
+    );
+  }
+}
+
+export default MovieList;
