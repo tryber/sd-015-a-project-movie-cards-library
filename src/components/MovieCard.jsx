60d21b5a401385388e29bd682a9c7a12bc709a89
Cla Dávila <clarice.coliveira@yahoo.com.br>
2021-10-11 20:17:43 -0300

    Requisito 7 - Renderize a imagem do filme dentro de uma tag img

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,46 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import '../data';
+import Rating from './Rating';
+import '../App.css';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
       <div>
-        <img src="" alt="" />
-        <h1>Movie Title</h1>
-        <h2>Movie Subtitle</h2>
-        <p>Storyline</p>
+        <img src={ imagePath } alt="" />
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+        <Rating rating={ rating } />
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+    rating: PropTypes.number,
+  }),
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  storyline: PropTypes.string,
+  imagePath: PropTypes.string,
+  rating: PropTypes.number,
+};
+MovieCard.defaultProps = {
+  movie: PropTypes.object,
+  title: '',
+  subtitle: '',
+  storyline: '',
+  imagePath: '',
+  rating: 0,
+};
 
 export default MovieCard;
