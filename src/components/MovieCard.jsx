06dd7b7613d656690fa5c03f241a6d8fc872e249
Leandro Basilio <leandroab07@gmail.com>
2021-10-11 02:39:06 -0300

    Adiciona requisito 12

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,6 +6,7 @@ import Rating from './Rating';
 class MovieCard extends React.Component {
   render() {
     const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
+    console.log(rating);
     return (
       <div>
         <img src={ imagePath } alt={ storyline } />
