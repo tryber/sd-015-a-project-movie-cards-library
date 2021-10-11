49244c96cd958b2a02fb0f4d62e04633fad971d6
Davio Lopes <davioplp@gmail.com>
2021-10-11 00:53:46 -0300

    final

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import data from './data';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
 
 function App() {
   return (
-    <Header class="movie-card-header" />
+    <div>
+      <Header class="movie-card-header" />
+      <MovieList movie={ data } />
+    </div>
   );
 }
 
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,24 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movie } = this.props;
+    return (
+      <div className="movie-list">
+        {
+          movie.map((element) => (
+            <MovieCard key={ element.title } movie={ element } />
+          ))
+        }
+      </div>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movie: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default MovieList;
