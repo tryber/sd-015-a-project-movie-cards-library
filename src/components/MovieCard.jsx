914aa681ca52da0693d07faf60b9b459ce4b0a0a
antoniofortisneto <antoniofortis@gmail.com>
2021-10-12 18:00:38 -0300

    mirei no gato acertei o peixe

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,7 @@
 // implement MovieCard component here
 import React from 'react';
 import propTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
@@ -13,6 +14,7 @@ class MovieCard extends React.Component {
         <h4 className="movie-card-title">{title}</h4>
         <h5 className="movie-card-subtitle">{subtitle}</h5>
         <p className="movie-card-storyline">{storyline}</p>
+        <Rating Rating={ Rating } />
       </header>);
   }
 }
@@ -22,6 +24,7 @@ MovieCard.propTypes = {
     subtitle: propTypes.string,
     storyline: propTypes.string,
     imagePath: propTypes.string,
+    rating: propTypes.number,
   }).isRequired,
 };
 export default MovieCard;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,19 @@
 // implement Rating component here
+import React from 'react';
+import propTypes from 'prop-types'
+class Rating extends React.Component {
+  render() {
+      const { rat } = this.props;
+    return (
+    <h3 className="rating">{ rat }</h3>
+    );
+  }
+}
+Rating.propTypes = {
+    rating: propTypes.number,
+};
+Rating.defaultProps = {
+    rating: -1,
+};
+
+export default Rating;
