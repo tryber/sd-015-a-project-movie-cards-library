fb50975b264b179144ca750c4c3665188fd2e83e
antoniofortisneto <antoniofortis@gmail.com>
2021-10-12 14:32:20 -0300

    indo

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,17 +4,23 @@ import propTypes from 'Prop-types';
 
 class MovieCard extends React.Component {
   render() {
-    const { movie: { imagePath } } = this.props;
+    const { movie: { title, subtitle, storyline, imagePath } } = this.props;
+
     return (
       <header>
         <h1>Movie Cards Library</h1>
-        <img src={ imagePath } alt="img the movie" />
-
+        <img className="movie-card-img" src={ imagePath } alt="img the movie" />
+        <h4 className="movie-card-title">{title}</h4>
+        <h5 className='movie-card-subtitle'>{subtitle}</h5>
+        <p className='movie-card-storyline'>{storyline}</p>
       </header>);
   }
 }
 MovieCard.propTypes = {
   Movie: propTypes.shape({
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  storyline: PropTypes.string,
   imagePath: propTypes.string,
   }).isRequired,
 };
