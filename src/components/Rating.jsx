f731eda5a45af53008793861a444ecc6134b04d1
antoniofortisneto <antoniofortis@gmail.com>
2021-10-12 18:06:30 -0300

    ta quase

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,19 +1,16 @@
 // implement Rating component here
 import React from 'react';
-import propTypes from 'prop-types'
+import propTypes from 'prop-types';
+
 class Rating extends React.Component {
   render() {
-      const { rat } = this.props;
-    return (
+  const { rat } = this.props;
+  return (
     <h3 className="rating">{ rat }</h3>
     );
   }
 }
-Rating.propTypes = {
-    rating: propTypes.number,
-};
-Rating.defaultProps = {
-    rating: -1,
-};
-
+  Rating.propTypes = {
+  rat: propTypes.number,
+}.isRequired;
 export default Rating;
