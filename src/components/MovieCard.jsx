f3124f704e2898323a4ddb90a9aeffe1b4f272e5
Odenir Zanoni Júnior <zanoni.odenir@gmail.com>
2021-10-12 11:36:08 -0300

    test

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    return (<div><Rating /></div>);
+    const { movie } = this.props;
+    return (
+      <div>
+        <img src={ movie.imagePath } alt="Card filme" />
+        <h4>{ movie.title }</h4>
+        <h5>{ movie.subtitle }</h5>
+        <p>{ movie.storyline }</p>
+        <Rating />
+      </div>
+    );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.objectOf().isRequired,
+};
 
 export default MovieCard;
