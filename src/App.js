715447c1770b7af5fec80fac9a7776813ae34cd0
Odenir Zanoni Júnior <zanoni.odenir@gmail.com>
2021-10-12 11:29:57 -0300

    Correçao lint

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import './App.css';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
+import data from './data';
 import Rating from './components/Rating';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <MovieList />
+      <MovieList movies={ data } />
       <Rating />
     </div>
   );
