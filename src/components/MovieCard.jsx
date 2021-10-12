731b71f999fabe2fc1a73d18f47db2274193c199
antoniofortisneto <antoniofortis@gmail.com>
2021-10-12 12:41:59 -0300

    a 7 foi agora

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 // implement MovieCard component here
 import React from 'react';
+import propTypes from 'Prop-types';
 
 class MovieCard extends React.Component {
   render() {
@@ -7,10 +8,14 @@ class MovieCard extends React.Component {
     return (
       <header>
         <h1>Movie Cards Library</h1>
-        <img src={ imagePath } alt="" />
+        <img src={ imagePath } alt="img the movie" />
 
       </header>);
   }
 }
-
+MovieCard.propTypes = {
+    Movie: propTypes.shape({
+    imagePath: propTypes.string,
+    }).isRequired,    
+}
 export default MovieCard;
