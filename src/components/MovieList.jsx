ca61eec7c1c483d7cc056188730edec757731144
antoniofortisneto <antoniofortis@gmail.com>
2021-10-12 09:15:21 -0300

    foi mais alguns

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,14 @@
 // implement MovieList component here
+import MovieCard from './MovieCard';
 import React from 'react';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
       <header>
         <h1>Movie Cards Library</h1>
+        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
       </header>);
   }
 }
