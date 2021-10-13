2ab7fc35cce23d26a77121f4c013c0de539ab958
Caio César <caiocesarmcruz@gmail.com>
2021-10-12 21:06:45 -0300

    requisitos 8 e 9

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,8 +6,8 @@ class MovieCard extends React.Component {
     return (
       movies.map((movie, index) => (
         <section key={ index }>
-          <h2>{ movie.title }</h2>
-          <p>{ movie.subtitle }</p>
+          <h4>{ movie.title }</h4>
+          <h5>{ movie.subtitle }</h5>
           <p>{ movie.storyline }</p>
           <img src={ movie.imagePath } alt="Foto do filme { movie.title }" />
           <p>{ movie.rating }</p>
