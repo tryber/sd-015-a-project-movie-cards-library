be2cb74cc9c9be8f513b1eaed0a9bd7ee1122eb6
Vitor <tutornews2@gmail.com>
2021-10-13 10:02:22 -0300

    Adiciona requesito 3 e outros

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
+import MovieCard from './components/MovieCard';
 
 function App() {
   return (
     <div className="App">
       {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
       <Header />
+      <MovieList />
+      <MovieCard />
     </div>
   );
 }
