f3661dd57dedd56142809b5788224edfb9359df1
Vitor <tutornews2@gmail.com>
2021-10-13 10:47:52 -0300

    Adiciona requesito 11.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import './App.css';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
 import MovieCard from './components/MovieCard';
+import Rating from './components/Rating';
 
 function App() {
   return (
     <div className="App">
       {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
       <Header />
-      <MovieList />
+      <MovieList movies={ movies } />
       <MovieCard />
+      <Rating />
     </div>
   );
 }
