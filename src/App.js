2ff49a68d1d0614caa37b602012bf6f66d323246
Vitor <tutornews2@gmail.com>
2021-10-13 12:11:58 -0300

    Adiciona requesitos

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
 import MovieList from './components/MovieList';
 import MovieCard from './components/MovieCard';
 import Rating from './components/Rating';
+import movies from './data';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
       <Header />
-      <MovieList movies={ movies } />
       <MovieCard />
+      <MovieList movies={ movies } />
       <Rating />
     </div>
   );
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 class MovieList extends React.Component {
   render() {
-    return <div>{console.log(this.props)}</div>;
+    return console.log(this.props);
   }
 }
 
