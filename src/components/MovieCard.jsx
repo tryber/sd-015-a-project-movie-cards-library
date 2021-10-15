c6cceff7f33453b806aaf1957745c831708107dd
Eliana Nunes <elianadnunes@gmail.com>
2021-10-14 21:09:13 -0300

    Ajustes lint ooo//

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,7 +7,7 @@ class MovieCard extends React.Component {
     const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
     return (
       <div className="movie-card">
-        <img src={ imagePath } className="movie-card-image" />
+        <img src={ imagePath } className="movie-card-image" alt="movie-img" />
         <div className="movie-card-body">
           <h4 className="movie-card-title">{title}</h4>
           <h5 className="movie-card-subtitle">{subtitle}</h5>
