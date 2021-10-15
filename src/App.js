31deb10b5381e89df984205a180793c4ad63a2c3
Vini_Vergani <vinivergani95@gmail.com>
2021-10-15 15:31:27 -0300

    Adiciona requisito 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+// import Card from './components/MovieCard';
+// import List from './components/MovieList';
+// import Rating from './components/Rating';
+
 
 function App() {
   return (
     <div className="App">
-      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
-      Hora de codar! 🚀🚀✍️👨‍💻👩‍💻💪
+     <Header />
     </div>
   );
 }
