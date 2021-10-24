fc68ae5afec6ed56235888a972b33ef1888b871a
Vini_Vergani <vinivergani95@gmail.com>
2021-10-24 17:23:42 -0300

    Adiciona requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 // import movies from '../data';
 
 class MovieList extends React.Component {
-
+  render() {
+    const { movie } = this.props;
+    return (
+      <div>
+        {movie}
+      </div>
+    )
+  }
 }
 
 export default MovieList;
