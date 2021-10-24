f1cb02c329d4bd90cf53b9c39e87ddace38b806b
Vini_Vergani <vinivergani95@gmail.com>
2021-10-24 19:19:38 -0300

    Adiciona lint requisitos finais

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Rating from './Rating';
 import propTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { imagePath, title, subtitle, storyline, rating} = movie;
+    const { imagePath, title, subtitle, storyline, rating } = movie;
     return (
       <div key={ title }>
         <img src={ imagePath } alt={ title } />
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
 class Rating extends React.Component {
-render() {
-    const { rating } =this.props;
-    return(
-        <span>{ rating }</span>
-    )
+  render() {
+    const { rating } = this.props;
+    return (
+      <span>{rating}</span>
+    );
+  }
+}
 
-}
-}
+Rating.propTypes = {
+  rating: propTypes.string,
+}.isRequired;
 
 export default Rating;
