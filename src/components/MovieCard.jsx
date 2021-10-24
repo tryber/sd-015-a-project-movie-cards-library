927f8f89700ae5da8ae4bfc8eea2a76f822c3bbf
Vini_Vergani <vinivergani95@gmail.com>
2021-10-24 18:10:09 -0300

    Teste requisitos refatorados

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import propTypes from 'prop-types';
 
+
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
     return (
-      <div>
-        {movie}
+      <div key= { movie.title }>
+        <p>{movie.title}</p>
+        <p>{movie.subtitle}</p>
+        <p>{movie.storyline}</p>
+        <img src= {  imagePath }/>
       </div>
     );
   }
 }
 
 MovieCard.propTypes = {
-  movie: propTypes.arrayOf(propTypes.object).isRequired,
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  storyline: PropTypes.string,
+  imagePath: PropTypes.string,
+  movie: PropTypes.instanceOf(Object), //Conforme material do distribuido disponivel em https://pt-br.reactjs.org/docs/typechecking-with-proptypes.html
 };
 
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
-// import MovieCard from './MovieCard';
-// import movies from '../data';
+import MovieCard from './MovieCard';
+import movies from '../data';
 
 class MovieList extends React.Component {
-  render() {
-    const { movie } = this.props;
-    return (
-      <div>
-        {movie}
-      </div>
-    );
-  }
+render() {
+  return (
+    <div>
+      {movies.map((name) => (
+        <MovieCard 
+        movie = { name }
+        key = {name.title}
+        />
+      ))}
+    </div>
+  )
 }
-
-MovieList.propTypes = {
-  movie: propTypes.arrayOf(propTypes.object).isRequired,
-};
+}
 
 export default MovieList;
