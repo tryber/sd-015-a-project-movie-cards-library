41f7a58571353893c8b6e42ea102a10782cc96fe
Vini_Vergani <vinivergani95@gmail.com>
2021-10-24 16:55:37 -0300

    teste req 6

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
-
+  render() {
+    const { movie } = this.props;
+    return (
+      <div>
+        {movie}
+      </div>
+    );
+  }
 }
+
+MovieCard.PropTypes = {
+  movie: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieCard;
